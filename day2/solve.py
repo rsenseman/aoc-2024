b887983a8bcb5c84dd201c597bc9cdf34100f949
Rob Senseman <robert.senseman@gmail.com>
2024-12-02 12:17:57 -0700

    day 2 part 2

--- day2/solve.py
+++ day2/solve.py
@@ -23,7 +23,6 @@ class Level:
     def from_line(cls, line: List[int]):
         return cls(line)
 
-    @property
     def is_monotonic(self):
         deltas = [x-y for x,y in zip(self.data[1:], self.data[:-1])]
 
@@ -35,7 +34,6 @@ class Level:
             all(map(is_negative, deltas))
         )
 
-    @property
     def are_deltas_in_range(self):
         deltas = [abs(x-y) for x,y in zip(self.data[1:], self.data[:-1])]
 
@@ -49,7 +47,86 @@ class Level:
     
     @property
     def is_safe(self):
-        return self.is_monotonic and self.are_deltas_in_range
+        return self.is_monotonic() and self.are_deltas_in_range()
+    
+class LevelWithProblemDampener(Level):
+    def is_monotonic_problem(self, delta, sign:str):
+        if sign == 'positive':
+            check_function = lambda v: v > 0
+        elif sign == 'negative':
+            check_function = lambda v: v < 0
+        else:
+            raise Exception('sign must be `positive` or `negative`')
+        
+        return not check_function(delta)
+
+    def is_scale_problem(self, delta):
+        delta_abs = abs(delta)
+
+        return not (
+            (delta_abs <= DELTA_MAX) and (delta_abs >= DELTA_MIN)
+        )
+    
+
+
+    @property
+    def is_safe_forward(self):
+        MAX_DAMPS = 1
+
+        for directionality in ['positive', 'negative']:
+            print()
+            steamroll_counter = 0
+
+            current = self.data[0] 
+            for next in self.data[1:]:
+                is_monotonic_problem = self.is_monotonic_problem(next-current, directionality)
+                is_scale_problem = self.is_scale_problem(next-current)
+
+                print(steamroll_counter, is_monotonic_problem, is_scale_problem)
+                if is_monotonic_problem or is_scale_problem:
+                    steamroll_counter += 1
+                    if steamroll_counter > MAX_DAMPS:
+                        break
+                    else:
+                        continue
+                else:
+                    current = next # only basic datatypes can be reassigned without getting miffed here
+            else:
+                return True
+            
+        return False
+    
+    @property
+    def is_safe_backward(self):
+        MAX_DAMPS = 1
+
+        for directionality in ['positive', 'negative']:
+            print()
+            steamroll_counter = 0
+
+            current = self.data[-1] 
+            for next in self.data[-2::-1]:
+                is_monotonic_problem = self.is_monotonic_problem(next-current, directionality)
+                is_scale_problem = self.is_scale_problem(next-current)
+
+                print(steamroll_counter, is_monotonic_problem, is_scale_problem)
+                if is_monotonic_problem or is_scale_problem:
+                    steamroll_counter += 1
+                    if steamroll_counter > MAX_DAMPS:
+                        break
+                    else:
+                        continue
+                else:
+                    current = next # only basic datatypes can be reassigned without getting miffed here
+            else:
+                return True
+            
+        return False
+
+    @property
+    def is_safe(self):
+        print('--------')
+        return self.is_safe_forward or self.is_safe_backward
     
 def solve_part1(data):
     levels = [Level.from_line(line) for line in data]
@@ -57,25 +134,32 @@ def solve_part1(data):
     return sum(int(level.is_safe) for level in levels)
 
 def solve_part2(data):
-    pass
+    num_safe = 0
+    for i, line in enumerate(data):
+        level = LevelWithProblemDampener.from_line(line)
+        is_safe = level.is_safe
+        print(f'{i+1}: {is_safe}')
+        if is_safe:
+            num_safe += 1
+    return num_safe
 
 if __name__ == '__main__':
-    data_dirty = get_data('./data_test.txt')
-    data = clean_data(data_dirty)
-    answer = solve_part1(data)
-    print(answer)
+    # data_dirty = get_data('./data_test.txt')
+    # data = clean_data(data_dirty)
+    # answer = solve_part1(data)
+    # print(answer)
 
-    data_dirty = get_data()
-    data = clean_data(data_dirty)
-    answer = solve_part1(data)
-    print(answer)
+    # data_dirty = get_data()
+    # data = clean_data(data_dirty)
+    # answer = solve_part1(data)
+    # print(answer)
 
     # data_dirty = get_data('./data_test.txt')
     # data = clean_data(data_dirty)
     # answer = solve_part2(data)
     # print(answer)
 
-    # data_dirty = get_data()
-    # data = clean_data(data_dirty)
-    # answer = solve_part2(data)
-    # print(answer)
+    data_dirty = get_data()
+    data = clean_data(data_dirty)
+    answer = solve_part2(data)
+    print(answer)
